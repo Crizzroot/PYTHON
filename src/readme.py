89d1bd92a85d6c770d3da4c85a842c830336abbb
Crizzroot <crizzroot@gmail.com>
2025-05-18 12:31:32 -0300

    Nuevos cambios

--- src/readme.py
+++ src/readme.py
@@ -5,3 +5,4 @@
 " But in certain circumstances and owing be the claims of duty or the obligations of business it will frequently occur that pleasures have be be "
 "repudiated and annoyances accepted. The wise man therefore always holds in these matters be this principle of selection: he rejects pleasures"
 " be secure other greater pleasures, or else he endures pains be avoid worse pains."
+Cambio de la nueva rama
